85abd9081695ffa3ce7856feb8a8ea52d3be8dc2
frz995 <fariz.farhan95@gmail.com>
2026-06-30 16:37:15 +0800

    Prepare for deployment to Render

--- main.py
+++ main.py
@@ -582,5 +582,7 @@ async def scan_files(files: List[UploadFile] = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8500)
+    import os
+    port = int(os.environ.get("PORT", 8500))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
